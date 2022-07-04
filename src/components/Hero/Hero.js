57e315d0b1e986b4a01e85efd55612eff66b958c
Maxmillian Ogbuabor <maximilianogbuabor@gmail.com>
2022-07-04 19:07:11 +0100

    Updating the Landing page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,8 +3,23 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        start here
+    <div>
+     <section class="container">
+        <div class="row">
+          <div class="col_1">
+            <div class="hero_text">
+              <h2>FACULTY OF ENGINEERING</h2>
+              <h1>Department of <span>Electronics</span> and <span>Computer</span> Engineering</h1>
+              <img src="./src/assests/images/vector.png" id="vector" alt="vector"/>
+              <p>A plug into an Engineering digital library,<br class="hide_on_mobile"/> Let's study.</p>
+              <a href="" class="common_red_btn">GET STARTED</a>
+            </div>
+          </div>
+          <div class="col_2">
+            <img src="./src/assests/images/image 1.png" alt="img1"/>
+          </div>
+        </div>	
+      </section>
     </div>
   )
 }
