9b61e0bab85b449b2b67378ed16d1116032abbf2
Maxmillian Ogbuabor <maximilianogbuabor@gmail.com>
2022-07-04 20:38:17 +0100

    Finished Landing Page

--- src/components/Newscenter/Newscenter.js
+++ src/components/Newscenter/Newscenter.js
@@ -3,7 +3,22 @@ import './Newscenter.css'
 
 const Newscenter = () => {
   return (
-    <div>Newscenter</div>
+    <div>
+      <section class="container">
+        <div class="row">
+          <div class="col_1">
+            <div class="news_text">
+              <h1>Stay <span>updated</span> everyday and everytime</h1>
+              <p>Get steady info and news, lecture timetable updates, newly uploaded pdfs and more</p>
+              <a href="" class="common_white_btn">NEWS CENTER</a>
+            </div>
+          </div>
+          <div class="col_2">
+            <img src="./src/assests/images/image 4.png" alt="img4"/>
+          </div>
+        </div>	
+      </section>
+    </div>
   )
 }
 
