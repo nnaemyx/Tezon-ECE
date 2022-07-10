4c2d9a47045b2ab0d83998021b620f7125383c28
Maxmillian Ogbuabor <maximilianogbuabor@gmail.com>
2022-07-10 21:32:05 +0100

    Updated the dom

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,25 +6,6 @@ import Vector2 from './Vector 2.png'
 const Footer = () => {
   return (
     <div>
-      <section className="last_section">
-        <div className="container">
-          <div className="last_section_text">
-            <h1>Get access to <span>everything</span> now</h1>
-            <img src={Vector2} alt='vector'/>
-            <a href="google.com" className="common_red_btn">SIGN UP</a>
-          </div>
-        </div>
-      </section>
-      <footer>
-          <div className="footer_content">
-            <img src={Tezonlogo} alt="logo"/>
-            <div className="footer_text">
-              <h1>TEZON</h1>
-              <p>This is the project of the Tezon Team, an 8 man squad from the 200lvl students of <br class="hide_on_mobile"/>Electronics and Computer Engineering</p>
-              <div className="find_us">
-                <a href="google.com">Contact Us</a>
-                <a href="google.com">About Us</a>
-              </div>
       <section>
         <summary>
           <h2>Get access to everything <span>now</span> <img src="src/assests/images/Vector 2.png"/> <a href="">Sign Up</a></h2>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar/Navbar'
 import Hero from '../components/Hero/Hero'
 import Ebooks from '../components/E-books/Ebooks'
 import Newscenter from '../components/Newscenter/Newscenter'
-import Results from '../components/Results/Results'
 import Footer from '../components/Footer/Footer'
 
 const Home = () => {
@@ -13,7 +12,6 @@ const Home = () => {
       <Hero/>
       <Ebooks/>
       <Newscenter/>
-      <Results/>
       <Footer/>
     </div>
   )
